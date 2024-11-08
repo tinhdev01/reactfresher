020d4f8964c4d8487fc02b19dcb82638ca8271ce
TINH DEV <tinhdev@gmail.com>
2024-11-08 10:31:52 +0700

    fix home back axios

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -48,7 +48,7 @@ instance.interceptors.response.use(function (response) {
     if (error.config && error.response && +error.response.status === 400
         && error.config.url === "/api/v1/auth/refresh"
     ) {
-        const localhost = "http://localhost:3000/"
+        const localhost = "https://frontendcv.tinhit.online"
         if (window.location.href === localhost) {
             //
         }
